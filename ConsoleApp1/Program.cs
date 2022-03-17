52289123135d30f0b432e0c98d6d672fa448a345
paquito05 <samuelchamuco2001@gmail.com>
2022-03-17 18:24:10 -0500

    Lab01 de DAEA

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Raiz()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("La raiz cuadrada de {0} es :{1}", i, Math.Sqrt(i));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Procedimientos y funciones";
+            string opcion;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] Suma de dos números");
+                Console.WriteLine("[2] Imprimir la raíz cuadrada de los 10 primeros números enteros");
+                Console.WriteLine("[3] Resta de dos números");
+                Console.WriteLine("[4] Multiplicacion de dos números");
+                Console.WriteLine("[5] Division de dos números");
+                Console.WriteLine("[6] Conversion de temperatura C° a F°");
+                Console.WriteLine("[7] Conversion de temperatura F° a C°");
+                Console.WriteLine("[0] Salir");
+                Console.WriteLine("Ingrese una opción y presione ENTER");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el primer número");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a, b, Suma(a, b));
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("Calculando...");
+                        Raiz();
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("Ingrese el primer número");
+                        int a3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        int b3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a3, b3, Suma(a3, b3));
+                        Console.ReadKey();
+                        break;
+                }
+            } while (!opcion.Equals("0"));
+
+        }
+        /*
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Programa lab 01");
+         Raiz();
+         Console.ReadKey();
+         Console.ReadKey();
+         }
+        */
+    }
+}
