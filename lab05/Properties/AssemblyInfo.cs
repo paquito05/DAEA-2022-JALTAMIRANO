8eb4127f9d112591b8fa22dd8c51cb6c9960e435
paquito05 <samuelchamuco2001@gmail.com>
2022-04-23 23:05:07 -0500

    lab 05

--- lab05/Properties/AssemblyInfo.cs
+++ lab05/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("lab03")]
+[assembly: AssemblyTitle("lab05")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lab03")]
+[assembly: AssemblyProduct("lab05")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("66e253bc-6315-4aaf-949b-01f13fa4b872")]
+[assembly: Guid("172380a1-9563-45db-97fb-34fb1f7aa367")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
---
+++ lab05/manPerson.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace lab05
+{
+    public partial class manPerson : Form
+    {
+
+        SqlConnection conn;
+        public manPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void manPerson_Load(object sender, EventArgs e)
+        {
+
+            String str = "Server=DESKTOP-D3IMFSS\\LOCAL; Database=School; Integrated Security = true;";
+            conn = new SqlConnection(str);
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            String sql = "select * from Person";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+            dgvListado.DataSource = dt;
+            dgvListado.Refresh();
+            conn.Close();
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string sp = "InsertPerson";
+            SqlCommand cmd = new SqlCommand(sp, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+            cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+            cmd.Parameters.AddWithValue("@HireDate", txtHireDate.Value);
+            cmd.Parameters.AddWithValue("@EnrollmentDate", txtEnrollmentDate.Value);
+
+            int codigo = Convert.ToInt32(cmd.ExecuteScalar());
+            MessageBox.Show("Se ha registrado nueva persona con el codigo : " + codigo);
+            conn.Close();
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            String sq = "UpdatePerson";
+
+            SqlCommand cmd = new SqlCommand(sq, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@PersonID", txtPersonID.Text);
+            cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+            cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+            cmd.Parameters.AddWithValue("@HireDate", txtHireDate.Value);
+            cmd.Parameters.AddWithValue("@EnrollmentDate", txtEnrollmentDate.Value);
+
+            int resultado = cmd.ExecuteNonQuery();
+            if (resultado > 0)
+            {
+                MessageBox.Show("Se ha modificado el registro correctamente");
+            }
+
+            conn.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            String sq = "DeletePerson";
+
+            SqlCommand cmd = new SqlCommand(sq, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@PersonID", txtPersonID.Text);
+
+            try
+            {
+                int resultado = cmd.ExecuteNonQuery();
+
+                if (resultado > 0)
+                {
+                    MessageBox.Show("Se ha eliminado el registro correctamente");
+                }
+
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error al Eliminar : " + ex.ToString());
+            }
+        }
+
+        private void dgvListado_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvListado.SelectedRows.Count > 0)
+            {
+                txtPersonID.Text = dgvListado.SelectedRows[0].Cells[0].Value.ToString();
+                txtLastName.Text = dgvListado.SelectedRows[0].Cells[1].Value.ToString();
+                txtFirstName.Text = dgvListado.SelectedRows[0].Cells[2].Value.ToString();
+                txtHireDate.Text = dgvListado.SelectedRows[0].Cells[3].Value.ToString();
+                txtEnrollmentDate.Text = dgvListado.SelectedRows[0].Cells[4].Value.ToString();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            if (txtFirstName.TextLength > 0)
+            {
+                String sqN = "BuscarPersonalNombre";
+
+                SqlCommand cmd = new SqlCommand(sqN, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+
+                try
+                {
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    dgvListado.DataSource = dt;
+                    dgvListado.Refresh();
+                    conn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error al Eliminar : " + ex.ToString());
+                }
+
+            }
+
+            if (txtPersonID.TextLength > 0)
+            {
+                String sqN = "BuscarPersonaCodigo";
+
+                SqlCommand cmd = new SqlCommand(sqN, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@PersonID", Int32.Parse(txtPersonID.Text));
+
+                try
+                {
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    dgvListado.DataSource = dt;
+                    dgvListado.Refresh();
+                    conn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error al Eliminar : " + ex.ToString());
+                }
+            }
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtPersonID.Text = "";
+            txtLastName.Text = "";
+            txtFirstName.Text = "";
+            txtHireDate.Text = "";
+            txtEnrollmentDate.Text = "";
+
+
+        }
+    }
+}
