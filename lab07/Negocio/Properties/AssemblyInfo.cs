b810dad75d67857ff5a7cc35f86e305b40df81da
paquito05 <samuelchamuco2001@gmail.com>
2022-05-09 11:32:23 -0500

    lab07

--- lab07/Negocio/Properties/AssemblyInfo.cs
+++ lab07/Negocio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("lab06")]
+[assembly: AssemblyTitle("Negocio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lab06")]
+[assembly: AssemblyProduct("Negocio")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("5e37c840-398b-42e6-8797-c7d6ba5dcafc")]
+[assembly: Guid("1fd00135-2127-4d06-8098-b7274b0b768c")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
---
+++ lab07/Presentacion/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        void Listar()
+        {
+            DataTable dt = new DataTable();
+            clsNEGPerson np = new clsNEGPerson();
+            dt = np.GetAll();
+
+            dgDatos.DataSource = dt;
+            dgDatos.Refresh();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            DataTable dt = new DataTable();
+            clsNEGPerson np = new clsNEGPerson();
+            string txtBuscar = txtSearch.Text.Trim();
+
+            try
+            {
+                dt = np.GetBuscar(txtBuscar);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en el form : " + ex.ToString());
+            }
+
+
+            dgDatos.DataSource = dt;
+            dgDatos.Refresh();
+
+        }
+
+
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+    }
+}
