d25a87a12e92e1f71dc946a35d92d263ee818d7f
paquito05 <samuelchamuco2001@gmail.com>
2022-06-05 16:30:14 -0500

    lab12

---
+++ Lab12/Controllers/PersonaController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab12.Models;
+
+namespace Lab12.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+
+            personas.Add(new Persona { PersonaId = 1,
+                Nombre =" John",
+                Apellido="Sanchez",
+                FechaNaci= Convert.ToDateTime("2001-11-05"),
+                Direccion ="San Andres 5etapa Mz E' Lote 4",
+                Email ="john.altamirano@tecsup.edu.pe"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaId = 2,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                FechaNaci = Convert.ToDateTime("1982-02-14"),
+                Direccion = "Av. Los manzanos 101",
+                Email = "carlos@gmail.com"
+            });
+
+            personas.Add(new Persona
+            {
+                PersonaId = 3,
+                Nombre = " Maria",
+                Apellido = "Salas",
+                FechaNaci = Convert.ToDateTime("1995-10-28"),
+                Direccion = "Av. Progreso 325",
+                Email = "maria@gmail.com"
+            });
+            return View(personas);
+
+
+        }
+    }
+}
--- SchoolApp/SchoolWEBB/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolWEBB.Global" Language="C#" %>
