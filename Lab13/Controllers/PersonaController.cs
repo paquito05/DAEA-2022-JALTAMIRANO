6d4dc4fecaf002155e4075b2aa6668b630286b8e
paquito05 <samuelchamuco2001@gmail.com>
2022-06-12 22:24:44 -0500

    lab13

---
+++ Lab13/Controllers/PersonaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab13.Models;
+
+namespace Lab13.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaId = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av.Evergreen 123",
+                FechaNaci = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaId = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNaci = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaId = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av.Los Manzanos 101",
+                FechaNaci = Convert.ToDateTime("1982-02-14"),
+                Email = "juan@mail.com"
+            });
+            return View(personas);
+        }
+
+        public ActionResult Mostrar(int id)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona
+            {
+                PersonaId = 1,
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Direccion = "Av.Evergreen 123",
+                FechaNaci = Convert.ToDateTime("1997-11-07"),
+                Email = "juan@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaId = 2,
+                Nombre = "Maria",
+                Apellido = "Salas",
+                Direccion = "Av. Progreso 325",
+                FechaNaci = Convert.ToDateTime("1995-10-28"),
+                Email = "maria@mail.com"
+            });
+            personas.Add(new Persona
+            {
+                PersonaId = 3,
+                Nombre = "Carlos",
+                Apellido = "Martinez",
+                Direccion = "Av.Los Manzanos 101",
+                FechaNaci = Convert.ToDateTime("1982-02-14"),
+                Email = "juan@mail.com"
+            });
+
+            Persona persona = (from p in personas
+                               where p.PersonaId == id
+                               select p).FirstOrDefault();
+
+            return View(persona);
+        }
+    }
+}
---
+++ Lab13/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab13.Models
+{
+    public class Persona
+    {
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNaci { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+}
