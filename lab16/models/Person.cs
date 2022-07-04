9fa3e796ec9025704b67e78113f10ba283002954
paquito05 <samuelchamuco2001@gmail.com>
2022-07-03 23:41:52 -0500

    lab16

---
+++ lab16/models/Person.cs
@@ -0,0 +1,18 @@
+using System;
+ 
+namespace persons.Models
+{
+    public class person
+    {
+        public int? PersonID {get; set;}
+ 
+        public string? LastName {get;set;}
+ 
+        public string? FirstName {get; set;}
+ 
+        public DateTime? HireDate {get; set;}
+ 
+        public DateTime? EnrollmentDate {get; set;}
+ 
+    }
+}
